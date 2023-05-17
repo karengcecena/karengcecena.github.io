4e912d3ab2fdd7a5f9c57d481f272838d82d260b
karengcecena <karengcecena@gmail.com>
2023-05-17 07:57:02 -0700

    Added clarifying comments

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 'use strict';
 
+// Function to retrieve a DOM element by its ID
 const $ = function(id) {
     return document.getElementById(id);
 };
 
+// Function to show an element by setting its display style to 'block'
 const show = function(id) {
     $(id).style.display = 'block';
 };
 
+// Function to hide an element by setting its display style to 'none'
 const hide = function(id) {
     $(id).style.display = 'none';
 };
